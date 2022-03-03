ba15a2b9306063ae1ab4f74273de738748cf5ff4
Haseeb Iqbal <haseebiqbal33@gmail.com>
2022-03-03 22:45:23 +0500

    protect dashboard by redirecting to the login page if user is null

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,21 @@
-function dashboard() {
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+function Dashboard() {
+
+    const navigate = useNavigate()
+    const { user } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login')
+        }
+    }, [user, navigate])
+
     return (
         <div>dashboard</div>
     )
 }
 
-export default dashboard
+export default Dashboard
